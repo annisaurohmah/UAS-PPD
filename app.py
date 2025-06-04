108877275f06455677d970987f24310412e8dec4
LENOVO <annisaurohmah@mail.ugm.ac.id>
2025-06-04 14:02:27 +0700

    strealit UI

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import joblib
 import numpy as np
+import pandas as pd
 
 # Load the trained model
 model = joblib.load('rf_model.sav')
 
 # Judul aplikasi
 st.title("Prediksi Harga Rumah (Random Forest Regressor)")
+st.image("None", use_column_width=True)
+st.write("Aplikasi ini memprediksi harga rumah berdasarkan beberapa fitur seperti tanggal transaksi, usia rumah, jarak ke MRT, jumlah convenience store, dan koordinat geografis (latitude dan longitude).")
 
 # Form input dari pengguna
 st.header("Masukkan Data Rumah")
@@ -19,7 +22,23 @@ X5 = st.number_input("X5 - Latitude", format="%.6f")
 X6 = st.number_input("X6 - Longitude", format="%.6f")
 
 # Prediksi ketika tombol diklik
+
+
 if st.button("Prediksi Harga"):
+    data_dict = {
+    "X1 - Transaction Date": [X1],
+    "X2 - House Age": [X2],
+    "X3 - Distance to MRT": [X3],
+    "X4 - Convenience Stores": [X4],
+    "X5 - Latitude": [X5],
+    "X6 - Longitude": [X6]
+}
+    st.subheader("Data yang Dimasukkan")
+    st.table(pd.DataFrame(data_dict))
+
     input_data = np.array([[X1, X2, X3, X4, X5, X6]])
     prediction = model.predict(input_data)[0]
     st.success(f"Perkiraan harga rumah per unit area: {prediction:.2f}")
+
+    df_map = pd.DataFrame({'lat': [X5], 'lon': [X6]})
+    st.map(df_map)
